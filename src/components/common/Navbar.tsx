b6594f5c1a97dabd582c524404a9210268615aeb
nahyan0077 <nahyanmp4@gmail.com>
2024-09-28 10:57:46 +0530

    feat: initial commit

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "@/assets/Logo.svg"; 
+import { Button } from "@/components/ui/button"; 
+import { IoIosArrowDown } from "react-icons/io";
+
+export const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-black text-white p-6">
+      <div className="container mx-auto flex justify-between items-center">
+
+        <div className="flex items-center">
+          <img src={logo} alt="Atma Studios" className="h-8 mr-4" />
+        </div>
+
+        <div className="hidden md:flex space-x-8 text-sm">
+            <div className="flex items-center gap-2" >
+                <span> <IoIosArrowDown /> </span>
+                <a href="#" className="hover:text-gray-300">  Courses</a> 
+            </div>
+            <div className="flex items-center gap-2" >
+                <span> <IoIosArrowDown /> </span>
+                <a href="#" className="hover:text-gray-300">  Events</a>
+            </div>
+            <div className="flex items-center gap-2" >
+                <span> <IoIosArrowDown /> </span>
+                <a href="#" className="hover:text-gray-300">  Community</a>
+            </div>
+            <div className="flex items-center gap-2" >
+                <a href="#" className="hover:text-gray-300">  For Teams</a>
+            </div>
+        </div>
+
+
+        <div>
+          <Button className="bg-white text-black hover:bg-gray-200">
+            Get in Touch
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "@/components/common/Navbar";
+import React from "react";
+
+
+export const Home: React.FC = () => {
+    return (
+        <>  
+            <Navbar />
+        </>
+    )
+}
